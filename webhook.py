cc42145be12c2ed8a10c2d80ba07bdfe562f1cc2
Derpitron <priya.aravamuthan@gmail.com>
2021-07-12 13:11:27 +0530

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,6 +1,8 @@
-from dhooks import Webhook
-import requests
 import time
+
+import requests
+from dhooks import Webhook
+
 hook = Webhook("https://discord.com/api/webhooks/863990102765207592/h8xfhL2ksmp-u-YI3uySYTwSUcV0cfezexbcjLNlytk9srp6hnnz3ZFbyyhaoSC1vbZk")
 
 url = 'http://api.openweathermap.org/data/2.5/weather?id=1264527&appid=0fd54c8d93dff4248d776d5644ef0946&units=metric'
@@ -10,14 +12,17 @@ weatherid = data['weather'][0]['id']
 cloud = data['clouds']['all']
 
 i = 0
-while i <= 1700:
-    i += 1
-    if i == 1700:
-        i = 0
+timer = 5
 
 def arive():
-    if i == 0:
-        hook.send("THE CLOUD HAS ARIVED\nJJJJJJJJJJJJJJJJ")
+    while i <= timer:
+        i += 1
+        time.sleep(1)
+        if i == timer:
+            i = 0
+            break
+        if i == 0:
+            hook.send("THE CLOUD HAS ARIVED\nJJJJJJJJJJJJJJJJ")
 
 #Thunderstorm
 if (weatherid == 200):
@@ -80,9 +85,9 @@ elif (weatherid == 531):
     arive()
 
 #Cloud Cover
-if ((cloud >= 70) and (cloud <= 89)):
+if ((cloud >= 70) and (cloud <= 85)):
     print("The cloud may be ariving - source: clouds")
-if ((cloud >= 90) and (cloud <= 100)):
+if ((cloud >= 86) and (cloud <= 100)):
     print("DARKNESS RISES")
 if ((weatherid == 500) or (weatherid == 501) or (weatherid == 502)):
     print("The cloud may be ariving - source: precipitation")
