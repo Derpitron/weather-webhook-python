0b43d805f090325b53f76be28632c161937ec30a
Derpitron <priya.aravamuthan@gmail.com>
2021-07-13 12:48:22 +0530

    ANJHAHHAHAHHAHAHAHHAHAHHAHAHAHAHHAHHAHAHAHHAHAHAHAHHAHAHAHHAHAHAH
    IT WORKS IT WORKS IT WORRRRRRRRRRKKKKKKKKKKKSSSSSSSSSSSSS

--- Python/webhook.py
+++ Python/webhook.py
@@ -8,23 +8,25 @@ url = 'http://api.openweathermap.org/data/2.5/weather?id=1264527&appid=0fd54c8d9
 while True:
 	res = requests.get(url)
 	data = res.json()
-	weatherID = data['weather'][0]['id']
-	cloud = data['clouds']['all']
+	weatherID1 = data['weather'][0]['id']
+	cloud1 = data['clouds']['all']
 	validWeather = ["200","201","202","210","211","212","221","230","231","232","300","301","302","310","311","312","313","314","321","503","504","511","520","521","522","530"]
 
 	def rainCheck():
-		if weatherID in validWeather:
+		if (weatherID1 in validWeather) and (weatherID2 in validWeather == 0):
 			hook.send("THE CLOUD HAS ARIVED")
 			hook.send("JJJJJJJJJJJJJJJJ")
 
 	def cloudCheck():
-    		if ((cloud >= 85) and (cloud <= 100)):
+    		if (((cloud1 >= 85) and (cloud1 <= 100)) and (weatherID1 in validWeather == 0) and ((cloud2 < 85))):
     			hook.send("DARKNESS RISES")	
 
 	def send():
 		rainCheck()
 		cloudCheck()
-		time.sleep(1200)
 		
 	send()
+	hook.send("ayo the pizza here")
 	time.sleep(300)
+	weatherID2 = weatherID1
+	cloud2 = cloud1
