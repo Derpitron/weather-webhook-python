a177a4adf6bca0fbd5d97a4a92d403c96b3dd2a3
Derpitron <priya.aravamuthan@gmail.com>
2021-07-13 21:14:01 +0530

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -4,11 +4,9 @@ import requests
 from dhooks import Webhook
 from dotenv import load_dotenv
 load_dotenv()
-api = os.getenv('API')
-hook = Webhook(os.getenv('HOOK'))
 x = 0
 while True:
-	response = requests.get(api)
+	response = requests.get(os.getenv('API'))
 	data = response.json()
 	rainData = data['rain']
 	isRaining = "rain" in rainData
@@ -16,8 +14,8 @@ while True:
 		isRaining2 = False
 	def rainCheck():
 		if ((isRaining) and (isRaining != isRaining2)):
-			hook.send("THE CLOUD HAS ARIVED")
-			hook.send("JJJJJJJJJJJJJJJJ")
+			Webhook(os.getenv('HOOK')).send("THE CLOUD HAS ARIVED")
+			Webhook(os.getenv('HOOK')).send("JJJJJJJJJJJJJJJJ")
 	def send():
 		rainCheck()
 	isRaining2 = isRaining
