87b2a1e709e587aa41db6119010099b39566fcb7
Derpitron <priya.aravamuthan@gmail.com>
2021-07-13 15:19:50 +0530

    r

--- webhook.py
+++ webhook.py
@@ -12,23 +12,23 @@ while True:
 	data = res.json()
 	weatherID1 = data['weather'][0]['id']
 	cloud1 = data['clouds']['all']
-	validWeather = ["200","201","202","210","211","212","221","230","231","232","300","301","302","310","311","312","313","314","321","503","504","511","520","521","522","530"]
+	validWeather = ["200","201","202","210","211","212","221","230","231","232","300","301","302","310","311","312","313","314","321","502","503","504","511","520","521","522","530"]
 
 	def rainCheck():
-		if ((weatherID1 in validWeather) and (weatherID2 in validWeather == False)):
+		if weatherID1 in validWeather:
 			hook.send("THE CLOUD HAS ARIVED")
 			hook.send("JJJJJJJJJJJJJJJJ")
 
 	def cloudCheck():
-    		if (((cloud1 >= 85) and (cloud1 <= 100)) and (weatherID1 in validWeather == False) and ((cloud2 < 85))):
+    		if (((cloud1 >= 85) and (cloud1 <= 100)) and (weatherID1 in validWeather == False)):
     			hook.send("DARKNESS RISES")	
 
 	def send():
 		rainCheck()
 		cloudCheck()
-	
-	hook.send(weatherID1)
 		
+	print(weatherID1)
+
 	send()
 	time.sleep(300)
 	weatherID2 = weatherID1
