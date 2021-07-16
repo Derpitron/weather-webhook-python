e871b06d954eaa24bcb431bcf2f44a3feaca4ce5
Derpitron <priya.aravamuthan@gmail.com>
2021-07-16 19:16:51 +0530

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -15,22 +15,23 @@ try:
 	x = False
 	debug.send("Script started on " + gethostname() + " at " + getenv('TIMEZONE') + " - " + localtz.strftime('%H:%M:%S') + " with interval of `" + getenv('INTERVAL') + "` seconds")
 	while True:
-		response = get(getenv('API'))
+		response = get(getenv("API"))
 		data = response.json()
 		isRaining = "rain" in data
 		if isRaining:
 			if x == False:
 				isRainingTest = False
+				debug.send("x == false part")
 			def rainCheck():
-				if ((isRaining) and (isRaining != isRainingTest)):
+				if ((isRaining == True) and (isRaining != isRainingTest)):
 					hook.send("THE CLOUD HAS ARIVED\nJJJJJJJJJJJJJJJJ")
 			isRainingTest = isRaining
 			rainCheck()
 			x = True
-		if isRaining:
-			debug.send("It is currently raining according to OpenWeatherMap")
-		else:
-			debug.send("It is not currently raining according to OpenWeatherMap")
+			if isRaining:
+				debug.send("It is currently raining according to OpenWeatherMap")
+			else:
+				debug.send("It is not currently raining according to OpenWeatherMap")
 		time.sleep(int(getenv('INTERVAL')))
 except Exception:
 	error = format_exc()
