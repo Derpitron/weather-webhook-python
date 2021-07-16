13c988100a32dd52c81b3b441e5218c7df45a776
Derpitron <priya.aravamuthan@gmail.com>
2021-07-16 20:30:11 +0530

    indentation real

--- webhook.py
+++ webhook.py
@@ -1,12 +1,14 @@
 import time
+from datetime import datetime
 from os import getenv
-from requests import get
+from socket import gethostname
+from traceback import format_exc
+
 from dhooks import Webhook
 from dotenv import load_dotenv
-from socket import gethostname
-from datetime import datetime
 from pytz import timezone
-from traceback import format_exc
+from requests import get
+
 load_dotenv()
 debug = Webhook(getenv('DEBUG'))
 localtz = datetime.now(timezone(getenv('TIMEZONE')))
@@ -17,21 +19,18 @@ try:
 	while True:
 		response = get(getenv("API"))
 		data = response.json()
-		isRaining = "rain" in data
+		print('rain' in data)
+		isRaining = 'rain' in data
 		if isRaining:
 			if x == False:
 				isRainingTest = False
-				debug.send("x == false part")
+
 			def rainCheck():
 				if ((isRaining == True) and (isRaining != isRainingTest)):
 					hook.send("THE CLOUD HAS ARIVED\nJJJJJJJJJJJJJJJJ")
 			isRainingTest = isRaining
 			rainCheck()
 			x = True
-			if isRaining:
-				debug.send("It is currently raining according to OpenWeatherMap")
-			else:
-				debug.send("It is not currently raining according to OpenWeatherMap")
 		time.sleep(int(getenv('INTERVAL')))
 except Exception:
 	error = format_exc()
