5ee179b87a2977864fb08bd6b2cccf33d15304de
Derpitron <priya.aravamuthan@gmail.com>
2021-08-29 15:43:59 +0530

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -17,26 +17,28 @@ try:
 	firstRun = False
 	debug.send("Script started on " + gethostname() + " with process ID: `" + str(os.getpid()) + "` at " + os.getenv('TIMEZONE') + " - " + localtz.strftime('%H:%M:%S') + " with interval of `" + os.getenv('INTERVAL') + "` seconds")
 	while True:
+		#Setting up the variables
 		data1 = get(os.getenv("API1")).json()
 		data2 = get(os.getenv("API2")).json()
 		data3 = get(os.getenv("API3")).json()
-		isRaining1 = 'rain' in data1
-		isRaining2 = 'rain' in data2
-		isRaining3 = 'rain' in data3
 		if ('rain' in data1):
-			isRaining = 
+			isRaining = 'rain' in data1
+			rainFallQuantity = data1['rain']['1h']
 		if ('rain' in data2):
-			isRaining = isRaining2
+			isRaining = 'rain' in data2
+			rainFallQuantity = data2['rain']['1h']
 		if ('rain' in data3):
-			isRaining = isRaining3
+			isRaining = 'rain' in data3
+			rainFallQuantity = data3['rain']['1h']
 
+		#The meat and potatoes of this program
 		if (isRaining == True):
 			if firstRun == False:
 				isRainingTest = False
 			if (isRaining != isRainingTest):
-				if ( >= 1.50):
+				if (rainFallQuantity >= os.getenv('RAINQUANTITY')): #If the rainfall quantity is equal to or more than 1.50mm
 					hook.send("THE CLOUD HAS ARIVED\nJJJJJJJJJJJJJJJJ")
-			isRainingTest = isRaining1
+			isRainingTest = isRaining
 			firstRun = True
 		time.sleep(int(os.getenv('INTERVAL')))
 
