9e8f0746364b5a135b36b5a51cf3a876c69de89c
Derpitron <priya.aravamuthan@gmail.com>
2021-10-16 16:49:40 +0530

    if name ==  main thingy
    +used the `if __name__ == "__main__"` convention
    
    +added some helpful comments
    
    +used the any() function
    
    +changed firstRun variable's initial value to make sense
    
    +changed isRainingTest to isRainingLastIter, which explains

--- webhook.py
+++ webhook.py
@@ -9,42 +9,45 @@ from dotenv import load_dotenv
 from pytz import timezone
 from requests import get
 
-load_dotenv()
-debug = Webhook(os.getenv('DEBUG'))
-localtz = datetime.now(timezone(os.getenv('TIMEZONE')))
-try:
-	hook = Webhook(os.getenv('HOOK'))
-	firstRun = False
-	debug.send("Script started on " + gethostname() + " with process ID: `" + str(os.getpid()) + "` at " + os.getenv('TIMEZONE') + " - " + localtz.strftime('%H:%M:%S') + " with interval of `" + os.getenv('INTERVAL') + "` seconds")
-	while True:
-		#Setting up the variables
-		data1 = get(os.getenv("API1")).json()
-		data2 = get(os.getenv("API2")).json()
-		data3 = get(os.getenv("API3")).json()
+def main():
+	load_dotenv()
+	debug = Webhook(os.getenv('DEBUG'))
+	localtz = datetime.now(timezone(os.getenv('TIMEZONE')))
+	try:
+		hook = Webhook(os.getenv('HOOK'))
+		firstRun = True #When the script is run for the first time
+		debug.send("Script started on " + gethostname() + " with process ID: `" + str(os.getpid()) + "` at " + os.getenv('TIMEZONE') + " - " + localtz.strftime('%H:%M:%S') + " with interval of `" + os.getenv('INTERVAL') + "` seconds")
+		while True:
+			#Setting up the variables
+			data1 = get(os.getenv("API1")).json()
+			data2 = get(os.getenv("API2")).json()
+			data3 = get(os.getenv("API3")).json()
+			rainData = ['data1', 'data2', 'data3']
+			isRaining = any(rainData)
+			#rainFallQuantities of each weather source
+			if ('rain' in data1):
+				rainFallQuantity = data1['rain']['1h']
+			elif ('rain' in data2):
+				rainFallQuantity = data2['rain']['1h']
+			elif ('rain' in data3):
+				rainFallQuantity = data3['rain']['1h']
 
-		if ('rain' in data1):
-			isRaining = 'rain' in data1
-			rainFallQuantity = data1['rain']['1h']
-		elif ('rain' in data2):
-			isRaining = 'rain' in data2
-			rainFallQuantity = data2['rain']['1h']
-		elif ('rain' in data3):
-			isRaining = 'rain' in data3
-			rainFallQuantity = data2['rain']['1h']
-		else:
-			continue
-		#The main part of this program
-		if (isRaining == True):
-			if firstRun == False:
-				isRainingTest = False
-			if (isRaining != isRainingTest):
-				if (rainFallQuantity >= 0.30): #If the rainfall quantity is equal to or more than 0.30mm
-					hook.send("THE CLOUD HAS ARIVED\nJJJJJJJJJJJJJJJJ")
-			isRainingTest = isRaining
-			firstRun = True
-		time.sleep(int(os.getenv('INTERVAL')))
+			#The main part of the program
+			if (isRaining == True):
+				if firstRun == True:
+					isRainingLastIter = False
+				if (isRaining != isRainingLastIter):
+					if (rainFallQuantity >= 0.30): #If the rainfall quantity is equal to or more than 0.30mm
+						hook.send("THE CLOUD HAS ARIVED\nJJJJJJJJJJJJJJJJ")
+				isRainingLastIter = isRaining
+				firstRun = False
+			time.sleep(int(os.getenv('INTERVAL')))
 
-except Exception:
-	error = format_exc()
-	debug.send("Script failed on " + gethostname() + " at " + "`" + os.getenv('TIMEZONE') + "`" + localtz.strftime('%H:%M:%S'))
-	debug.send("```" + error + "```")
+	#Error Handling
+	except Exception:
+		error = format_exc()
+		debug.send("Script failed on " + gethostname() + " at " + "`" + os.getenv('TIMEZONE') + "`" + localtz.strftime('%H:%M:%S'))
+		debug.send("```" + error + "```")
+
+if __name__ == "__main__":
+	main()
