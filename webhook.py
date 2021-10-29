607353917e6deda3651743e36ce8f0fc1b56a611
Derpitron <priya.aravamuthan@gmail.com>
2021-10-29 23:51:29 +0530

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -17,13 +17,15 @@ def main():
 		hook = Webhook(os.getenv('HOOK'))
 		firstRun = True #When the script is run for the first time
 		debug.send("Script started on " + gethostname() + " with process ID: `" + str(os.getpid()) + "` at " + os.getenv('TIMEZONE') + " - " + localtz.strftime('%H:%M:%S') + " with interval of `" + os.getenv('INTERVAL') + "` seconds")
+		
 		while True:
 			#Setting up the variables
 			data1 = get(os.getenv("API1")).json()
 			data2 = get(os.getenv("API2")).json()
 			data3 = get(os.getenv("API3")).json()
-			rainData = ['data1', 'data2', 'data3']
+			rainData = [('rain' in data1), ('rain' in data2), ('rain' in data3)]
 			isRaining = any(rainData)
+			
 			#rainFallQuantities of each weather source
 			if ('rain' in data1):
 				rainFallQuantity = data1['rain']['1h']
